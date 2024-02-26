165d3709e3b41a39c82ef6ecf4dbc1580ae0ce18
DarkSla5her <95456926+DarkSla5her@users.noreply.github.com>
2024-02-26 22:20:45 +0000

    bootstrap and images

--- Django_finalyear_app/final_year_app/food_recommendation/urls.py
+++ Django_finalyear_app/final_year_app/food_recommendation/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('recommend/', views.food_recommendation, name='food_recommendation'),
+    # path('recommend/', views.food_recommendation, name='food_recommendation'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('homepage/', views.homepage, name='homepage'),
--- Django_finalyear_app/final_year_app/food_recommendation/views.py
+++ Django_finalyear_app/final_year_app/food_recommendation/views.py
@@ -6,6 +6,26 @@ from sklearn.neighbors import NearestNeighbors
 import numpy as np
 from django.conf import settings
 import os
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login as auth_login
+
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect('http://127.0.0.1:8000/')  
+    else:
+        form = AuthenticationForm()
+    return render(request, 'food_recommendation/login.html', {'form': form})
+
+
 def login(request):
     return render(request, 'food_recommendation/login.html')
 
@@ -16,9 +36,6 @@ def homepage(request):
      return render(request, 'food_recommendation/homepage.html')
 
 def recommend_food(request):
-     return render(request, 'food_recommendation/recommend_food.html')
-
-def food_recommendation(request):
     if request.method == 'POST':
         vegn = request.POST.get('vegn')
         cuisine = request.POST.get('cuisine')
@@ -52,6 +69,42 @@ def food_recommendation(request):
         return render(request, 'food_recommendation/recommendations.html', {'recommendations': recommendations})
     else:
         return render(request, 'food_recommendation/recommend_food.html')
+
+
+# def food_recommendation(request):
+#     if request.method == 'POST':
+#         vegn = request.POST.get('vegn')
+#         cuisine = request.POST.get('cuisine')
+#         val = int(request.POST.get('val'))
+
+#         # Construct absolute paths for the CSV files
+#         food_csv_path = os.path.join(settings.BASE_DIR, 'food_recommendation', 'food.csv')
+#         ratings_csv_path = os.path.join(settings.BASE_DIR, 'food_recommendation', 'ratings.csv')
+
+#         # Load data using absolute paths
+#         food = pd.read_csv(food_csv_path)
+#         ratings = pd.read_csv(ratings_csv_path)
+#         combined = pd.merge(ratings, food, on='Food_ID')
+
+#         # Filter data based on user preferences
+#         ans = combined.loc[(combined['C_Type'] == cuisine) & (combined['Veg_Non'] == vegn) & (combined['Rating'] >= val), ['Name', 'C_Type', 'Veg_Non']]
+#         names = ans['Name'].tolist()
+#         ans1 = np.unique(np.array(names))
+
+#         # Run recommender
+#         dataset = ratings.pivot_table(index='Food_ID', columns='User_ID', values='Rating')
+#         dataset.fillna(0, inplace=True)
+#         csr_dataset = csr_matrix(dataset.values)
+#         dataset.reset_index(inplace=True)
+
+#         model = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
+#         model.fit(csr_dataset)
+
+#         recommendations = food_recommendation_helper(ans1, food, dataset, model, csr_dataset)
+
+#         return render(request, 'food_recommendation/recommendations.html', {'recommendations': recommendations})
+#     else:
+#         return render(request, 'food_recommendation/recommend_food.html')
 
 
 
