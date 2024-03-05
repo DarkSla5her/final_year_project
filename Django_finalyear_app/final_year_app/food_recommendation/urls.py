b29357320bacc7aa374ff1fd1618148186f32d4f
DarkSla5her <95456926+DarkSla5her@users.noreply.github.com>
2024-03-05 17:54:50 +0000

    made drinks recommendation, account and fixed food recommendation

--- Django_finalyear_app/final_year_app/food_recommendation/urls.py
+++ Django_finalyear_app/final_year_app/food_recommendation/urls.py
@@ -2,15 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    # path('recommend/', views.food_recommendation, name='food_recommendation'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login'),
     path('signup/', views.signup, name='signup'),
-    # path('signingup/', views.signingup, name='signingup'),
     path('homepage/', views.homepage, name='homepage'),
     path('recommend_food/', views.recommend_food, name='recommend_food'),
     path('recommend_drink/', views.recommend_drink, name='recommend_drink'),
+    path('account/', views.account, name='account'),
 
-
-    path('register/', views.register, name='register')
     ]
--- Django_finalyear_app/final_year_app/food_recommendation/views.py
+++ Django_finalyear_app/final_year_app/food_recommendation/views.py
@@ -8,122 +8,97 @@ from django.conf import settings
 import os
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.views.decorators.csrf import csrf_exempt
-# from django.contrib.auth import authenticate, login as auth_login
 from .forms import SignUpForm
 from django.contrib.auth import login, logout, authenticate
-# from .models import User # Import your custom user model
-# from .forms import SignUpForm # Import your custom user form
+from django.contrib import messages
+import random
 
-
-def register(request):
-    form=  SignUpForm()
+def signup(request):
+    print("Signup view accessed")  # Confirm the view is being hit
     if request.method == 'POST':
-        form= SignUpForm()
+        print("Processing POST request")  # Confirm form submission
+        form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
-
-    context={'form':form}
-
-    return render(request, 'food_recommendation/register.html')
-
-
-
-
-
-# def signup(request):
-#     if request.method == 'POST':
-#         form = SignUpForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             # Redirect to success page or any other page
-#             return redirect('success')
-#     else:
-#         form = SignUpForm()
-#     return render(request, 'food_recommendation/signup.html', {'form': form})
-
-
-# @csrf_exempt
-# def signup(request):
-#     form= SignUpForm()
-#     if request.method == "POST":
-#         form = SignUpForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#     else:
-#         form = SignUpForm()
-#         return render(request, 'food_recommendation/signup.html', {'form': form})
-
-
-
-    # redirect_to = request.POST.get('next', '')  # Get the value of 'next' from POST data
-    # if request.method == "POST":
-    #     form = SignUpForm(request.POST)
-    #     if form.is_valid():
-    #         user = form.save()
-    #         login(request, user)
-    #         if redirect_to:  # Check if a redirection URL is provided
-    #             return redirect(redirect_to)  # Redirect to the provided URL
-    #         else:
-    #             return redirect("homepage")  # Default redirection to homepage
-    # else:
-    #     form = SignUpForm()
-    # return render(request, 'food_recommendation/signup.html', {'form': form})
+            print("Form is valid")
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Account created successfully')
+            return redirect('login')
+        else:
+            print("Form is not valid")
+            print(form.errors.as_data())  # More detailed form errors
+    else:
+        form = SignUpForm()
+    return render(request, 'food_recommendation/signup.html', {'form': form})
 
 
 @csrf_exempt
 def login_view(request):
-    if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect("home")
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('homepage')  # Redirect to the homepage
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
     else:
         form = AuthenticationForm()
-    return render(request, "login.html", {"form": form})
+    return render(request, 'food_recommendation/login.html', {'form': form})
+
 
 def logout_view(request):
     if request.method == "POST":
         logout(request)
         return redirect("home")
 
+def account(request):
+     return render(request, 'food_recommendation/account.html')
+ 
 
-# def signup_view(request):
-#     if request.method == 'POST':
-#         form = CustomUserCreationForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             user = form.save()
-#             return redirect('login')
-#     else:
-#         form = CustomUserCreationForm()
-#     return render(request, 'api/spa/signup.html', {'form': form})
-
-# def login_view(request):
-#     if request.method == 'POST':
-#         form = AuthenticationForm(request, data=request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data.get('password')
-#             user = authenticate(username=username, password=password)
-#             if user is not None:
-#                 auth_login(request, user)
-#                 return redirect('http://127.0.0.1:8000/')  
-#     else:
-#         form = AuthenticationForm()
-#     return render(request, 'food_recommendation/login.html', {'form': form})
-
-
-def login(request):
-    return render(request, 'food_recommendation/login.html')
-
-def signup(request):
-    return render(request, 'food_recommendation/signup.html')
 
 def homepage(request):
      return render(request, 'food_recommendation/homepage.html')
-
+ 
 def recommend_drink(request):
-     return render(request, 'food_recommendation/recommend_drink.html')
+    if request.method == 'POST':
+        alcoholic_or_non_alcoholic = request.POST.get('alcoholic_or_non_alcoholic')
+        drink_type = request.POST.get('drink_type')
+        wine_subtype = request.POST.get('wine_subtype')
+
+        # Read drinks data from CSV file
+        drinks = pd.read_csv("food_recommendation/drinks.csv")  # Update with the actual path
+
+        # Filter drinks based on user preferences
+        if alcoholic_or_non_alcoholic == 'Alcoholic':
+            if drink_type == 'Wine':
+                drinks = drinks[drinks['Type'] == wine_subtype]
+            else:
+                drinks = drinks[drinks['Type'] == drink_type]
+        else:
+            if drink_type == 'Soft':
+                drinks = drinks[drinks['Type'].str.contains('Soft', case=False)]
+            else:
+                drinks = drinks[drinks['Type'] == drink_type]
+
+        # Filtered drinks
+        filtered_drinks = drinks[['Name', 'Type']]
+        drink_names = filtered_drinks['Name'].tolist()
+
+        if drink_names:
+            recommended_drink = random.choice(drink_names)
+        else:
+            recommended_drink = "No drinks available for the selected type."
+
+        return render(request, 'food_recommendation/recommend_drink.html', {'recommended_drink': recommended_drink})
+
+    return render(request, 'food_recommendation/recommend_drink.html')
+#
 
 def recommend_food(request):
     if request.method == 'POST':
@@ -155,47 +130,11 @@ def recommend_food(request):
         model.fit(csr_dataset)
 
         recommendations = food_recommendation_helper(ans1, food, dataset, model, csr_dataset)
+        recommendations = random.choice(recommendations)
 
-        return render(request, 'food_recommendation/recommendations.html', {'recommendations': recommendations})
+        return render(request, 'food_recommendation/recommend_food.html', {'recommendations': recommendations})
     else:
         return render(request, 'food_recommendation/recommend_food.html')
-
-
-# def food_recommendation(request):
-#     if request.method == 'POST':
-#         vegn = request.POST.get('vegn')
-#         cuisine = request.POST.get('cuisine')
-#         val = int(request.POST.get('val'))
-
-#         # Construct absolute paths for the CSV files
-#         food_csv_path = os.path.join(settings.BASE_DIR, 'food_recommendation', 'food.csv')
-#         ratings_csv_path = os.path.join(settings.BASE_DIR, 'food_recommendation', 'ratings.csv')
-
-#         # Load data using absolute paths
-#         food = pd.read_csv(food_csv_path)
-#         ratings = pd.read_csv(ratings_csv_path)
-#         combined = pd.merge(ratings, food, on='Food_ID')
-
-#         # Filter data based on user preferences
-#         ans = combined.loc[(combined['C_Type'] == cuisine) & (combined['Veg_Non'] == vegn) & (combined['Rating'] >= val), ['Name', 'C_Type', 'Veg_Non']]
-#         names = ans['Name'].tolist()
-#         ans1 = np.unique(np.array(names))
-
-#         # Run recommender
-#         dataset = ratings.pivot_table(index='Food_ID', columns='User_ID', values='Rating')
-#         dataset.fillna(0, inplace=True)
-#         csr_dataset = csr_matrix(dataset.values)
-#         dataset.reset_index(inplace=True)
-
-#         model = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
-#         model.fit(csr_dataset)
-
-#         recommendations = food_recommendation_helper(ans1, food, dataset, model, csr_dataset)
-
-#         return render(request, 'food_recommendation/recommendations.html', {'recommendations': recommendations})
-#     else:
-#         return render(request, 'food_recommendation/recommend_food.html')
-
 
 
 def food_recommendation_helper(food_names, food_data, dataset, model, csr_dataset):
