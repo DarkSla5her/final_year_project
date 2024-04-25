dfde6cabfe3fd0fcd76cdf9bd8ac2470191741c7
DarkSla5her <95456926+DarkSla5her@users.noreply.github.com>
2024-04-25 04:11:06 +0100

    code comments

--- Django_finalyear_app/final_year_app/food_recommendation/views.py
+++ Django_finalyear_app/final_year_app/food_recommendation/views.py
@@ -63,65 +63,65 @@ def account(request):
 def homepage(request):
      return render(request, 'food_recommendation/homepage.html')
  
-def recommend_drink(request):
-    if request.method == 'POST':
+def recommend_drink(request):#the drinks function to recommend a drink, takes request as an argument
+    if request.method == 'POST':#checks if the request is post which means that it is submitted
         alcoholic_or_non_alcoholic = request.POST.get('alcoholic_or_non_alcoholic')
-        drink_type = request.POST.get('drink_type')
+        drink_type = request.POST.get('drink_type')#retrieves the values from the form
         wine_subtype = request.POST.get('wine_subtype')
 
-        # Read drinks data from CSV file
-        drinks = pd.read_csv("food_recommendation/drinks.csv")  # Update with the actual path
+        # Reads the drinks data from the CSV file
+        drinks = pd.read_csv("food_recommendation/drinks.csv")  
 
         # Filter drinks based on user preferences
-        if alcoholic_or_non_alcoholic == 'Alcoholic':
-            if drink_type == 'Wine':
+        if alcoholic_or_non_alcoholic == 'Alcoholic':#checks if user has selected alcoholic
+            if drink_type == 'Wine':#checks if user has selected wine, if they have they will be asked to select a subtype
                 drinks = drinks[drinks['Type'] == wine_subtype]
             else:
-                drinks = drinks[drinks['Type'] == drink_type]
+                drinks = drinks[drinks['Type'] == drink_type]#otherwise filters normally for other alcoholic drinks
         else:
-            if drink_type == 'Soft':
+            if drink_type == 'Soft':#checks if soft has been selected to select only the drinks that contain the word 'soft'
                 drinks = drinks[drinks['Type'].str.contains('Soft', case=False)]
             else:
                 drinks = drinks[drinks['Type'] == drink_type]
 
         # Filtered drinks
-        filtered_drinks = drinks[['Name', 'Type']]
-        drink_names = filtered_drinks['Name'].tolist()
+        filtered_drinks = drinks[['Name', 'Type']]#new data frame created containing name and type 
+        drink_names = filtered_drinks['Name'].tolist()#takes the name and converts it into a list
 
         if drink_names:
-            recommended_drink = random.choice(drink_names)
-        else:
-            recommended_drink = "No drinks available for the selected type."
+            recommended_drink = random.choice(drink_names)#checks if there are drinks in the drink_names list, if there are it is assigned to recommend_drink
+        else:#which uses a random in-built function to generate a recommendation
+            recommended_drink = "No drinks available for the selected type."#if nothing is available this is returned
 
-        return render(request, 'food_recommendation/recommend_drink.html', {'recommended_drink': recommended_drink})
+        return render(request, 'food_recommendation/recommend_drink.html', {'recommended_drink': recommended_drink})# whatever is in recommend_drink is shown to user
 
-    return render(request, 'food_recommendation/recommend_drink.html')
+    return render(request, 'food_recommendation/recommend_drink.html')#if method is not POST default render is returned
 
-def recommend_food(request):
-    if request.method == 'POST':
+def recommend_food(request):#function that takes request as an argument
+    if request.method == 'POST':#form in the frontend has been submitted
         course = request.POST.get('course').strip()
-        cuisine = request.POST.get('cuisine').strip()
-        allergies_str = request.POST.get('allergies').strip()
+        cuisine = request.POST.get('cuisine').strip()#gets the values from the form fields and removes
+        allergies_str = request.POST.get('allergies').strip()#any leading or trailing white spaces
         vegetarian = request.POST.get('vegetarian', None)
         meat = request.POST.get('meat', None)
 
-        # Convert allergies string to a list
+        # Convert allergies string to a list splitting it by commas and removing any empty strings
         allergies = [allergy.strip() for allergy in allergies_str.split(',') if allergy.strip()]
 
-        # Construct absolute paths for the CSV files
+        # Constructs the absolute path to the CSV files
         food_csv_path = os.path.join(settings.BASE_DIR, 'food_recommendation', 'food_dataset.csv')
 
-        # Load data using absolute paths
+        # Reads the dataset into a pandas data frame 
         food_data = pd.read_csv(food_csv_path)
 
-        # Strip whitespaces from the 'Food' and 'Meat' columns
+        # Strip whitespaces from specific columns in the food_data data frame
         food_data['Food'] = food_data['Food'].str.strip()
         food_data['Meat'] = food_data['Meat'].apply(lambda x: x.strip() if isinstance(x, str) else x)
         food_data['Type_of_course'] = food_data['Type_of_course'].str.strip()
         food_data['Cuisine'] = food_data['Cuisine'].str.strip()
         food_data['Is_vegetarian'] = food_data['Is_vegetarian'].str.strip()
 
-        # Filter data based on user preferences and allergies
+        # Filters the food_data data frame based on user course and cuisine
         ans = food_data[
                     (food_data['Type_of_course'] == course) &
                     (food_data['Cuisine'] == cuisine)
@@ -132,46 +132,46 @@ def recommend_food(request):
             ans = ans[ans['Is_vegetarian'] == vegetarian]
 
         # Filter by meat if specified
-        if meat:
+        if meat:#checks if the meat is specified in the meat column which can contain multiple meat types seperated by commas
             ans = ans[ans['Meat'].apply(lambda x: meat.lower() in str(x).lower().split(','))]
 
-        # Filter out any allergies
+        # Filter out any foods that contain the users specified allergies
         for allergy in allergies:
             ans = ans[~ans['Allergies'].str.contains(allergy, case=False, na=False)]
 
         if ans.empty:
-            print("No data found after filtering.")
+            print("No data found after filtering.")#checks if there are no recommendations left after filtering
             recommendations = "No recommendations found."
-        else:
+        else:#if there are recommendations calls this function to get a recommendation and randomly selects one
             recommendations = food_recommendation_helper(ans,ans['Food'].tolist(), food_data, allergies, cuisine, vegetarian, meat)
             if recommendations:
-                recommendations = random.choice(recommendations)
+                recommendations = random.choice(recommendations)#random used to generate a random recommendation
             else:
                 recommendations = "No recommendations found."
-
+        #renders the template and passes the recommendations and selected course values to be displayed 
         return render(request, 'food_recommendation/recommend_food.html', {'recommendations': recommendations, 'selected_course': course})
     else:
-        return render(request, 'food_recommendation/recommend_food.html')
+        return render(request, 'food_recommendation/recommend_food.html')#if method not POST
 
 
-def food_recommendation_helper(food_database,food_names, food_data, allergies, selected_cuisine, vegetarian, meat):
+def food_recommendation_helper(food_database,food_names, food_data, allergies, selected_cuisine, vegetarian, meat):#takes several arguments
     
-    # testing and debugging code
+    # testing and debugging code printed in terminal
     selected_food_data = food_database
     print("Testing...")
     print (selected_food_data)
 
-    # Check if there is data left after filtering
+    # Check if there is data left after filtering, returns empty list if it is
     if selected_food_data.empty:
         return []
 
-    # Create dummies for selected food data
+    # Creates dummies for the selected food column using one-hot encoding
     food_dummies = pd.get_dummies(selected_food_data['Food'])
     selected_food_data.reset_index(drop=True, inplace=True)
-    n_samples = len(food_dummies)
-    n_neighbors = min(11, n_samples)  # Adjust neighbors based on available samples
+    n_samples = len(food_dummies)#Calculates the number of samples and determines the number of neighbors to use in the NearestNeighbors model
+    n_neighbors = min(11, n_samples)  # Adjust neighbors based on available samples, max 11 neigbors
 
-    # Fit NearestNeighbors model
+    # If there are more than 1 sample, it initializes and fits a NearestNeighbors model using cosine similarity as the distance metric and brute-force algorithm
     if n_samples > 1:
         model = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=n_neighbors, n_jobs=-1)
         model.fit(food_dummies)
@@ -182,6 +182,8 @@ def food_recommendation_helper(food_database,food_names, food_data, allergies, s
     recommendations = []
 
     # Generate recommendations for each food name
+    #each food name in food_names, it finds the closest matches using the NearestNeighbors model 
+    #and adds them to the recommendations list if they are not in the allergies list.
     for food_name in food_names:
         FoodList = selected_food_data[selected_food_data['Food'].str.contains(food_name)]
         if not FoodList.empty:
@@ -199,9 +201,9 @@ def food_recommendation_helper(food_database,food_names, food_data, allergies, s
                         if matching_food['Food'] not in allergies and matching_food['Food'] not in recommendations:
                             recommendations.append(matching_food['Food'])
 
-    # Add any unmatched food items to recommendations
+    # Add any unmatched food items to the recommendations list 
     for food_name in food_names:
         if food_name not in recommendations:
             recommendations.append(food_name)
 
-    return recommendations
+    return recommendations #return the final list of recommendations
