e57b76b71063e8fcd71faa2dd551a7054e6b5b62
DarkSla5her <95456926+DarkSla5her@users.noreply.github.com>
2024-04-25 02:59:58 +0100

    removed unused comments

--- Django_finalyear_app/final_year_app/food_recommendation/models.py
+++ Django_finalyear_app/final_year_app/food_recommendation/models.py
@@ -3,14 +3,3 @@ from django.contrib.auth.models import AbstractBaseUser
 
 
 
-
-# class User(AbstractBaseUser):
-#     email = models.EmailField(unique=False, null=True, blank=True)
-#     first_name = models.CharField(max_length=50)
-#     last_name = models.CharField(max_length=50)
-
-#     USERNAME_FIELD = "email"
-#     REQUIRED_FIELDS = ["first_name", "last_name"]
-
-#     def __str__(self):
-#         return self.email
